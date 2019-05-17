0eaa9e621051685db777bcf42d13de6a1de82c4e
dinobaranasic <dino.baranasic8@gmail.com>
2019-05-17 14:23:35 +0200

    Prikaz veličina datoteka

--- DatotecniSustav01/Program.cs
+++ DatotecniSustav01/Program.cs
@@ -10,52 +10,38 @@ namespace DatotecniSustav01
             Console.WriteLine("C:");
             string direktorij = @"C:\";
             DirectoryInfo dirInfo = new DirectoryInfo(direktorij);
+
             var datoteke = dirInfo.GetFiles();
-
             float velicina = 0;
-            //maksimalni naziv
-            int nazivmax = 0;
 
-            //pomocu foreach trazimo najdulji naziv i spremamo ga u nazivmax
-            foreach (FileInfo d in datoteke)
-            {
-                if (d.FullName.Length > nazivmax)
-                {
-                    nazivmax = d.FullName.Length;
-                }
-            }
-
-            //pomocu PadRight metode kreiramo fleksibilnu širinu tablice prema nazivmax
-            Console.Write("+------------------+-------------+---------+--------+------+"); Console.Write("-".PadRight(nazivmax, '-')); Console.WriteLine("+");
-            Console.Write("| B                |          KB |      MB |     GB |   TB |"); Console.Write("Naziv Diska".PadRight(nazivmax)); Console.WriteLine("+");
-            Console.Write("+------------------+-------------+---------+--------+------+"); Console.Write("-".PadRight(nazivmax, '-')); Console.WriteLine("+");
-
+            Console.WriteLine("+------------------+-------------+---------+----------------------------------------------------------+");
+            Console.WriteLine("| Veličina       B |          KB |      MB |     GB |   TB | Nazivi datoteka                          |");
+            Console.WriteLine("+------------------+-------------+---------+--------+------+------------------------------------------+");
             foreach (FileInfo d in datoteke)
             {
                 velicina += d.Length;
-                Console.Write("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3, 3} GB | {4, 1} TB |",
+                Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3, 3} GB | {4, 1} TB | {5,40} |",
                     d.Length,
                     d.Length / 1024,
                     d.Length / (1024 * 1024),
                     d.Length / (1024 * 1024 * 1024),
-                    (d.Length / (1024 * 1024 * 1024)) / 1024);
-                    Console.Write("{0}".PadRight((nazivmax + 3) - d.FullName.Length), d.FullName); Console.WriteLine("|");
+                    (d.Length / (1024 * 1024 * 1024)) / (1024),
+                    d.FullName);
             }
-
-            Console.Write("+------------------+-------------+---------+--------+------+"); Console.Write("-".PadRight(nazivmax, '-')); Console.WriteLine("+");
-            Console.Write("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3, 3} GB | {4, 1} TB |",
+            
+            Console.WriteLine("+------------------+-------------+---------+--------+------+------------------------------------------+");
+            Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 4} MB | {3, 3} GB | {4, 1} TB |                                          |",
                 velicina,
                 velicina / 1024,
-                //zaokružujemo vrijednosti da bi nam stale unutar tablice
-                Math.Round(velicina / (1024 * 1024)),
-                Math.Round(velicina / (1024 * 1024 * 1024)),
-                Math.Round((velicina / (1024 * 1024 * 1024)) / (1024)));
+                velicina / (1024 * 1024),
+                velicina / (1024 * 1024 * 1024),
+                (velicina / (1024 * 1024 * 1024)) / (1024));
 
-            Console.WriteLine();
-            Console.Write("+------------------+-------------+---------+--------+------+"); Console.Write("-".PadRight(nazivmax, '-')); Console.WriteLine("+");
+            Console.WriteLine("+------------------+-------------+---------+--------+------+------------------------------------------+");
 
             Console.SetCursorPosition(1, 3);
             Console.Write(">");
+
             int brojRedova = datoteke.Length + 6;
 
             int cekanjeTreperenje = 500;
